001cc45464a1185f9547f5445eb7b3d0f82c61f0
leocorbur <113495542+leocorbur@users.noreply.github.com>
2023-10-27 09:31:55 -0500

    data_render

--- api_steam/routers/api.py
+++ api_steam/routers/api.py
@@ -4,30 +4,31 @@ from pathlib import Path
 
 router = APIRouter()
 
-data_folder = Path('../datasets')
+data_folder = Path('../data_render')
+
 
 data = {}
 
-# Archivos CSV
+'''# Archivos CSV
 files = ['game_genres.csv', 'genres.csv', 
-         'name_games.csv', 'reviews.csv']
+         'name_games.csv', 'reviews.csv']'''
 
 
 
 # Cargar las partes de play_time.csv y combinarlas en un solo DataFrame
 play_time_parts = []
 for i in range(1, 4):
-    play_time_file = f'play_time_{i}.csv'
+    play_time_file = f'query_{i}.csv'
     play_time_path = data_folder / play_time_file
     play_time_part = pd.read_csv(play_time_path)
     play_time_parts.append(play_time_part)
 
 play_time_combined = pd.concat(play_time_parts, ignore_index=True)
 
-# Cargar otros archivos CSV
+'''# Cargar otros archivos CSV
 for file in files:
     file_path = data_folder / file
-    data[file] = pd.read_csv(file_path)
+    data[file] = pd.read_csv(file_path)'''
 
 
 
@@ -58,7 +59,7 @@ async def get_anio_mas_horas_by_id(id_genre: int):
 '''
     
 
-# Ruta para obtener el anio con mas horas jugadas de un genre 
+'''# Ruta para obtener el anio con mas horas jugadas de un genre 
 @router.get("/anio_mas_horas/{Genre}")
 async def get_anio_mas_horas_by_genre(Genre: str):
 
@@ -81,5 +82,26 @@ async def get_anio_mas_horas_by_genre(Genre: str):
                 f"Año con más horas jugadas para el Género {Genre}": year_with_most_hours
             }
     else:
+            return {"error": "No se encontraron datos para el género especificado"}
+    '''
+
+# 01.1 Ruta para obtener el anio con mas horas jugadas de un genre 
+
+@router.get("/anio_mas_horas2/{Genre}")
+async def get_anio_mas_horas_by_genre(Genre: str):
+     
+     filtered_data = play_time_combined[play_time_combined['genre'] == Genre]
+
+     if not filtered_data.empty:
+            # Calcular las horas totales jugadas por año
+            hours_by_year = filtered_data.groupby(filtered_data['anio'])['playtime_forever'].sum()
+
+            # Encontrar el año con más horas jugadas
+            year_with_most_hours = hours_by_year.idxmax()
+
+            return {
+                f"Año con más horas jugadas para el Género {Genre}": int(year_with_most_hours)
+            }
+     else:
             return {"error": "No se encontraron datos para el género especificado"}
     
